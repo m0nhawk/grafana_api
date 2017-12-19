46c1595f1ecfdfb935b6dcfb302b1e9c9aeecb10
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2017-12-19 22:36:30 +0200

    quotes changed

--- grafana_api/grafana_api.py
+++ grafana_api/grafana_api.py
@@ -2,7 +2,7 @@ import requests
 
 
 class GrafanaAPI:
-    def __init__(self, auth, host="localhost", port=None, url_path_prefix="", protocol="http"):
+    def __init__(self, auth, host='localhost', port=None, url_path_prefix='', protocol='http'):
         self.auth = auth
         self.url_host = host
         self.url_port = port
@@ -11,16 +11,16 @@ class GrafanaAPI:
 
         def construct_api_url():
             params = {
-                "protocol": self.url_protocol,
-                "host": self.url_host,
-                "url_path_prefix": self.url_path_prefix,
+                'protocol': self.url_protocol,
+                'host': self.url_host,
+                'url_path_prefix': self.url_path_prefix,
             }
 
             if self.url_port is None:
-                url_pattern = "{protocol}://{host}/{url_path_prefix}api"
+                url_pattern = '{protocol}://{host}/{url_path_prefix}api'
             else:
-                params["port"] = self.url_port
-                url_pattern = "{protocol}://{host}:{port}/{url_path_prefix}api"
+                params['port'] = self.url_port
+                url_pattern = '{protocol}://{host}:{port}/{url_path_prefix}api'
 
             return url_pattern.format(**params)
 
@@ -28,7 +28,7 @@ class GrafanaAPI:
 
     def __getattr__(self, item):
         def __requests_run(url, json=None, headers=None):
-            __url = "%s%s" % (self.url, url)
+            __url = '%s%s' % (self.url, url)
             runner = getattr(requests, item.lower())
             r = runner(__url, json=json, headers=headers, auth=self.auth)
             return r

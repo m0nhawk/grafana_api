eddcde78c5d5c7131d2f037b0b718aaac2d17526
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2017-12-19 22:36:17 +0200

    .travis.yml testing

---
+++ test/test_grafana.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from grafana_api.grafana_face import GrafanaFace
+
+
+class GrafanaAPITest(unittest.TestCase):
+    @patch('grafana_api.grafana_api.GrafanaAPI.__getattr__')
+    def test_grafana_api(self, mock_get):
+        mock_get.return_value = Mock()
+        mock_get.return_value.return_value.content = """{
+  "email": "user@mygraf.com",
+  "name": "admin",
+  "login": "admin",
+  "theme": "light",
+  "orgId": 1,
+  "isGrafanaAdmin": true
+}"""
+        cli = GrafanaFace(('admin', 'admin'), host='localhost', url_path_prefix='', protocol='https')
+        cli.user_exist('test@test.com')
+
+
+if __name__ == '__main__':
+    unittest.main()

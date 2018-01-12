96078ddb52a98611e9b1278bb1ff95df63c04cc3
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2018-01-12 16:26:21 +0200

    0.2.2 version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='grafana_api',
-      version='0.2.1',
+      version='0.2.2',
       description='Yet another Python library for Grafana API',
       url='https://github.com/m0nhawk/grafana_api',
       author='Andrew Prokhorenkov',

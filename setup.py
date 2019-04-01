96a7d2a6932992705661412533a0d07e45836339
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2019-03-31 21:11:27 -0500

    chore(setup): more configurable Python package configuration

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from subprocess import check_output
+
+
+def get_version():
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode('utf-8').strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 with open('README.md') as file:
     long_description = file.read()
 
 setup(name='grafana_api',
-      version='0.5.2',
+      version=get_version(),
       description='Yet another Python library for Grafana API',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -12,12 +28,10 @@ setup(name='grafana_api',
       author='Andrew Prokhorenkov',
       author_email='andrew.prokhorenkov@gmail.com',
       license='MIT',
-      packages=[
-          'grafana_api',
-          'grafana_api.api'
-      ],
+      packages=find_packages(),
       install_requires=[
           'requests',
+          'pyyaml',
       ],
       classifiers=[
           'Development Status :: 3 - Alpha',

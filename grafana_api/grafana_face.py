4334a789c8dc2b3cf319a9d7810de5575aca1dd0
a.zubarev <a.zubarev@mac410rih.rambler.ramblermedia.com>
2019-04-17 16:33:26 +0300

    remove magic __getattr__

--- grafana_api/grafana_face.py
+++ grafana_api/grafana_face.py
@@ -1,13 +1,16 @@
 from .grafana_api import GrafanaAPI
-from .api import Base
+from .api import (Admin, Dashboard, Datasource, Folder, Organization, Organizations, Search, User, Users)
 
 
 class GrafanaFace:
     def __init__(self, auth, host="localhost", port=None, url_path_prefix="", protocol="http", verify=True):
-        self.api = GrafanaAPI(auth, host=host, port=port, url_path_prefix=url_path_prefix, protocol=protocol, verify=verify)
-
-    def __getattr__(self, item):
-        for cls in Base.__subclasses__():
-            if cls.__name__.lower() == item:
-                return cls(api=self.api)
-        return
+        api = GrafanaAPI(auth, host=host, port=port, url_path_prefix=url_path_prefix, protocol=protocol, verify=verify)
+        self.admin = Admin(api)
+        self.dashboard = Dashboard(api)
+        self.datasource = Datasource(api)
+        self.folder = Folder(api)
+        self.organization = Organization(api)
+        self.organizations = Organizations(api)
+        self.search = Search(api)
+        self.user = User(api)
+        self.users = Users(api)

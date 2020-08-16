e3f59a2ca6b268762a5b95703efdae7d20b06907
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2020-08-16 13:41:50 -0500

    feat: updates

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@
 # https://discuss.python.org/t/specification-of-editable-installation/1564/
 from setuptools import setup
 setup(
-    use_scm_version=True,
+    use_scm_version={
+        "local_scheme": "no-local-version",
+        "version_scheme": "python-simplified-semver",
+    },
 )
